8f28cf80ec5e330391995c76850ec95a40383cd0
WakeskaterX <WakeskaterStudio@gmail.com>
2014-01-26 11:26:56 -0500

    add an OSX Build

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -15,7 +15,7 @@ public class Title : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUI.DrawTexture (new Rect(-10f,-5f,775f,360f),main_tex);
+		GUI.DrawTexture (new Rect(-20f,-5f,Screen.width + 35f ,Screen.height + 15f),main_tex);
 		//GUI.Label(new Rect(Screen.width/2-200,Screen.height/2+100,400,200),"Evo Dash");
 
 		//GUI.Label (new Rect(Screen.width/2-200,Screen.height/2-100,400,200),"Press Enter");
